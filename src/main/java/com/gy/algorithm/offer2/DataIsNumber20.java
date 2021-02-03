720b1ffa115a8dfbf5cb583863d34c361ec24c83
leelipeng <leelipeng@didiglobal.com>
2021-02-03 12:16:35 +0800

    学习20题

---
+++ src/main/java/com/gy/algorithm/offer2/DataIsNumber20.java
@@ -0,0 +1,14 @@
+package com.gy.algorithm.offer2;
+
+/**
+ * Friendship is a sheltering tree.
+ *
+ * @author : leelipeng
+ * @date : 2021-02-03 12:16
+ */
+public class DataIsNumber20 {
+
+    public static void main(String[] args) {
+        // TODO 暂不实现.
+    }
+}
